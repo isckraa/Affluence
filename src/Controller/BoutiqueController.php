0c227be38dffad1d750a15cba3b2513383859f4d
Wolf Nathan <nathan-w@hotmail.fr>
2020-06-23 15:24:21 +0200

    Make create boutique function

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Boutique;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class BoutiqueController extends AbstractController
 {
@@ -17,5 +23,25 @@ class BoutiqueController extends AbstractController
         ]);
     }
 
-    
+    /**
+     * @Route("/boutique/create", name="boutique_create", methods={"POST"})
+     */
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator) {
+        $jsonRequest = $request->getContent();
+        try {
+            $boutique = $serializer->deserialize($jsonRequest, Boutique::class, 'json');
+            $errors = $validator->validate($boutique);
+            if(count($errors) > 0) {
+                return $this->json($errors, 400);
+            }
+            $em->persist($boutique);
+            $em->flush();
+            return $this->json($boutique, '201');
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage(),
+            ], 400);
+        }
+    }
 }
