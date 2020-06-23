229dd946d992c50bd3af4df14d9c6aad141f9219
Wolf Nathan <nathan-w@hotmail.fr>
2020-06-23 17:16:42 +0200

    Make functions to collect all stores, store by ID, by name or by postal code

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Boutique;
+use App\Repository\BoutiqueRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
@@ -41,6 +43,92 @@ class BoutiqueController extends AbstractController
             return $this->json([
                 'status' => 400,
                 'message' => $e->getMessage(),
+            ], 400);
+        }
+    }
+
+    /**
+     * @Route("/boutique/list", name="boutique_list", methods={"GET"})
+     */
+    public function list(BoutiqueRepository $boutiqueRepository, SerializerInterface $serializer)
+    {
+        return $this->json($boutiqueRepository->findAll(), 200, []);
+    }
+
+    /**
+     * @param BoutiqueRepository $boutiqueRepository
+     * @param SerializerInterface $serializer
+     * @Route("/boutique/list/id", name="boutique_list_id", methods={"GET", "POST"})
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function findById(BoutiqueRepository $boutiqueRepository, SerializerInterface $serializer, Request $request, ValidatorInterface $validator) {
+        $requestContent = $request->getContent();
+        /* try {
+            // $boutique = $serializer->deserialize($jsonRequest, Boutique::class, 'json');
+            var_dump($boutique);
+            $errors = $validator->validate($boutique);
+            if(count($errors) > 0) {
+                return $this->json($errors, 400);
+            }
+            return $this->json($boutiqueRepository->findOneBy(["id" => $boutique->getId()]), 200, []);
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        } */
+        $id = (int)$requestContent;
+        if ($id > 0) {
+            return $this->json($boutiqueRepository->findOneBy(["id" => $id]), 200, []);
+        }
+        return $this->json([
+            'status' => 400,
+            'message' => "Not good integer"
+        ], 400);
+    }
+
+    /**
+     * @param BoutiqueRepository $boutiqueRepository
+     * @param SerializerInterface $serializer
+     * @Route("/boutique/list/nom", name="boutique_list_nom", methods={"GET", "POST"})
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function findByNom(BoutiqueRepository $boutiqueRepository, SerializerInterface $serializer, Request $request, ValidatorInterface $validator) {
+        $jsonRequest = $request->getContent();
+        try {
+            $boutique = $serializer->deserialize($jsonRequest, Boutique::class, 'json');
+            $errors = $validator->validate($boutique);
+            if(count($errors) > 0) {
+                return $this->json($errors, 400);
+            }
+            return $this->json($boutiqueRepository->findBy(["nom" => $boutique->getNom()]), 200, []);
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    /**
+     * @param BoutiqueRepository $boutiqueRepository
+     * @param SerializerInterface $serializer
+     * @Route("/boutique/list/code_postal", name="boutique_list_code_postal", methods={"GET", "POST"})
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function findByCodePostal(BoutiqueRepository $boutiqueRepository, SerializerInterface $serializer, Request $request, ValidatorInterface $validator) {
+        $jsonRequest = $request->getContent();
+        try {
+            $boutique = $serializer->deserialize($jsonRequest, Boutique::class, 'json');
+            $errors = $validator->validate($boutique);
+            if(count($errors) > 0) {
+                return $this->json($errors, 400);
+            }
+            return $this->json($boutiqueRepository->findBy(["codePostal" => $boutique->getCodePostal()]), 200, []);
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
             ], 400);
         }
     }
