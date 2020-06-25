015f2c1e44d84077379a59514f6b68c8df600764
Wolf Nathan <nathan-w@hotmail.fr>
2020-06-25 15:18:54 +0200

    Install nelmio cors bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
